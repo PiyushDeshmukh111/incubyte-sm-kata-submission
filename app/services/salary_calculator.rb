211f39834c74b072c95af09814acfd950f6df271
Piyush Deshmukh <piyushdeshmukh479@gmail.com>
2026-03-22 16:33:42 +0530

    feat(salary): implement salary calculator (GREEN)

--- app/services/salary_calculator.rb
+++ app/services/salary_calculator.rb
@@ -1,17 +1,23 @@
 class SalaryCalculator
-    def self.call(employee)
-      gross = employee.salary.to_f
-  
-      tds =
-        case employee.country
-        when "India"
-          gross * 0.10
-        when "United States"
-          gross * 0.12
-        else
-          0
-        end
-  
-      { gross_salary: gross, tds: tds, net_salary: gross - tds }
-    end
+  def self.call(employee)
+    gross = employee.salary.to_f
+
+    tds =
+      case employee.country
+      when "India"
+        gross * 0.10
+      when "United States"
+        gross * 0.12
+      else
+        0
+      end
+
+    tds = tds.round(2)   
+
+    {
+      gross_salary: gross,
+      tds: tds,
+      net_salary: (gross - tds).round(2)
+    }
   end
+end
