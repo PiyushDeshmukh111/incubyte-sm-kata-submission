d16cc254acb64eda7c0ef86a0b0ccdb73f5701f1
Piyush Deshmukh <piyushdeshmukh479@gmail.com>
2026-03-22 16:22:56 +0530

    refactor(employee-api): extract before_action

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,4 +1,6 @@
 class EmployeesController < ApplicationController
+    before_action :set_employee, only: %i[show update destroy]
+
     def index
       render json: Employee.all
     end
@@ -28,7 +30,11 @@ class EmployeesController < ApplicationController
     end
   
     private
-  
+
+    def set_employee
+      @employee = Employee.find(params[:id])
+    end
+
     def employee_params
       params.require(:employee).permit(:full_name, :job_title, :country, :salary)
     end
